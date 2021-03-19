a43484ff7960995419b38475b9f7f56c61260fc8
younes ait m'ha <younesaitmha@gmail.com>
2021-03-19 15:52:30 +0100

    update routes

--- market/routes.py
+++ market/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from market import app, db
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @app.route('/')
@@ -10,10 +10,38 @@ from flask_login import login_user, logout_user
 def home_page():
     return render_template('index.html')
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
+@login_required
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+    purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
+    if request.method == "POST":
+        #Purchase Item Logic
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price}$", category='success')
+            else:
+                flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}!", category='danger')
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
+
+
+        return redirect(url_for('market_page'))
+
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, selling_form=selling_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -22,10 +50,12 @@ def register_page():
         user_to_create = User(username=form.username.data, email_address=form.email_address.data, password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}:
         for error_message in form.errors.values():
-            flash(error_message, category='danger')
+            flash(error_message[0], category='danger')
     return render_template('register.html', form = form )
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -35,17 +65,17 @@ def login_page():
         attempted_user = User.query.filter_by(username=form.username.data).first()
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
-            flash([f'Success! You are logged in as: {attempted_user.username}'], category='success')
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
             return redirect(url_for('market_page'))
         else:
-            flash(['Username and password are not match! Please try again'], category='danger')
+            flash('Username and password are not match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout_page():
     logout_user()
-    flash(["You have been logged out!"], category='info')
+    flash("You have been logged out!", category='info')
     return redirect(url_for("home_page"))
 
 @app.errorhandler(404)
