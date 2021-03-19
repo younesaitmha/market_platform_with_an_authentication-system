f560ce26189097f0ec59a781437f0f96af658820
younes ait m'ha <younesaitmha@gmail.com>
2021-03-19 15:52:06 +0100

    update models

--- market/models.py
+++ market/models.py
@@ -34,6 +34,12 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+
+    def can_sell(self, item_obj):
+        return item_obj in self.items
+
 
 
 class Item(db.Model):
@@ -46,3 +52,13 @@ class Item(db.Model):
 
     def __repr__(self) -> str:
         return f'Item({self.name}, {self.price})'
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
+
+    def sell(self, user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
