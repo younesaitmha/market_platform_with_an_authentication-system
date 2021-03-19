d761b0bb2ec5aca350cfb4935291eb8efe41d3dd
younes ait m'ha <younesaitmha@gmail.com>
2021-03-19 15:51:26 +0100

    update forms

--- market/forms.py
+++ market/forms.py
@@ -25,3 +25,9 @@ class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Purchase Item!')
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Sell Item!')
